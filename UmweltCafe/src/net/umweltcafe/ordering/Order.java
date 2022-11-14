3981ffc81b272d4789f402f41dd3db9e79cd16fa
Lukas Reil <lukas_reil@>
2022-11-14 23:00:37 +0100

    20221114

--- UmweltCafe/src/net/umweltcafe/ordering/Order.java
+++ UmweltCafe/src/net/umweltcafe/ordering/Order.java
@@ -30,31 +30,31 @@ public class Order {
 		this.time = time.getDayOfMonth() + "." + time.getMonthValue() + "." + time.getYear() + " " + time.getHour()
 				+ ":" + time.getMinute() + ":" + time.getSecond();
 	}
-	
+
 	public float getPrice() {
 		float sum = 0;
-		
-		for(String id : ids) {
+
+		for (String id : ids) {
 			sum += Config.price(id);
 		}
-		
+
 		return sum;
 	}
-	
+
 	public void addToCSV(CSV csv) {
-		csv.addRow(new String[] {"Order","", String.format("%.2f", getPrice()), time});
-		for(String id : ids) {
-			csv.addRow(new String[] {
-				"Item", id, String.format("%.2f", Config.price(id))
-		});
+		csv.addRow(new String[] { "Order", "", String.format("%.2f", getPrice()), time });
+		for (String id : ids) {
+			csv.addRow(new String[] { "Item", id, String.format("%.2f", Config.price(id)) });
 		}
 	}
-	
 
-	
-	public static Order parseOrder(String string) {
+	public static Order parseOrder(CSV csv, int start, int end) {
 		ArrayList<String> ids = new ArrayList<String>();
-		String time = "";
+		String time = csv.getCell(start, 3);
+		
+		for(int i = start; i <= end; i++) {
+			ids.add(csv.getCell(i, 1));
+		}
 		
 		return new Order(ids, time);
 	}
